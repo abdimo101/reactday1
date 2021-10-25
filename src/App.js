f5ff27456f42eec185231e194ddaac02c8e64e10
Abdi Abdulle <cph-aa394@cphbusiness.dk>
2021-10-25 11:07:30 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import obj from './file2';
+import { males } from './file2';
+import { females } from './file2';
+
+
+//function Persons(props){
+ // const {males, females} = props;
+  //return <p>{males}, {females}</p>
+  //}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const person = obj;
+const personMale = males;
+const personFemale = females;
+const {firstName, email} = person;
+
+const maleArr = personMale[0] + ", " + personMale[1];
+const femaleArr = personFemale[0] + ", " + personFemale[1];
+const bothGendersArr = maleArr + ", "+femaleArr;
+console.log(bothGendersArr)
+  return(
+  <div className="App">  
+  <h2>Ex 2</h2>
+  <p>{firstName}</p>
+  <p>{email}</p> 
+  <p>{bothGendersArr}</p>
+  </div>
+    );
 }
 
 export default App;
